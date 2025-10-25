1f57cb6964a3112222ef10af2a92c08461acc968
Hayakawa Shinji <hayakawa@ai-r.kyoto.jp>
2025-10-25 14:04:14 +0900

    update:ログイン画面をSubMainから呼び出すように修正

---
+++ RecordedDelivery/Program.vb
@@ -0,0 +1,10 @@
+﻿Module Program
+
+    Sub Main()
+        ' 任意のフォームを起動
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New OperatorInputForm()) ' ← 起動したいフォームに変更
+    End Sub
+
+End Module
